c830c2b5c9cdae55104107c6a831c25a5476208a
Славин Сергей Станиславович <slavin.sergey.s@gmail.com>
2026-09-24 19:35:02 +0800

    Fix battleground creation after bot commander removal

--- src/server/game/Maps/MapInstanced.cpp
+++ src/server/game/Maps/MapInstanced.cpp
@@ -269,7 +269,6 @@ BattlegroundMap* MapInstanced::CreateBattleground(uint32 InstanceId, Battlegroun
     ASSERT(map->IsBattlegroundOrArena());
     map->SetBG(bg);
     bg->SetBgMap(map);
-    map->InitCommander();
 
     m_InstancedMaps[InstanceId] = map;
     return map;
